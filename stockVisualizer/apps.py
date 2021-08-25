5ec3a3736beca0aebe9246ad5d20decfafc9fd0d
robbieswan <64027345+robbieswan@users.noreply.github.com>
2021-08-25 11:11:39 -0600

    Delete stockVisualizer directory

--- stockVisualizer/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockvisualizerConfig(AppConfig):
-    name = 'stockVisualizer'
--- stockVisualizer/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class StockData(models.Model):
-    symbol = models.TextField(null=True)
-    data = models.TextField(null=True)
